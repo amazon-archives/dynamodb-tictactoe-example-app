f6fa1541c05b5f98352a8393303e7e826734a10a
Peter Bronez <pbronez@deepmile.com>
2014-09-18 16:27:16 -0400

    Fixed global reference error to setupDynamoDB functions

--- dynamodb/connectionManager.py
+++ dynamodb/connectionManager.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from setupDynamoDB          import getDynamoDBConnection, createGamesTable
 from boto.dynamodb2.table   import Table
 from uuid                   import uuid4
 
